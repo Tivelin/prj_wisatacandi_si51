27871096a4526b29a48e1717a4e6542707dc48c7
Tivelin Hendry <tivelinhendry@mhs.mdp.ac.id>
2023-11-13 19:23:40 +0700

    Grid View (final)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prj_wisatacandi_si51/data/candi_data.dart';
 import 'package:prj_wisatacandi_si51/models/candi.dart';
+import 'package:prj_wisatacandi_si51/widgets/item_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,8 +11,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Candi candi = candiList[index];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +25,8 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(8),
         itemCount: candiList.length,
         itemBuilder: (context, index) {
-          return ItemCard();
+          Candi candi = candiList[index];
+          return ItemCard(candi: candi);
         },
       ),
       // TODO : 3. buat itemcard sebagai return value dari Gridview.builder
